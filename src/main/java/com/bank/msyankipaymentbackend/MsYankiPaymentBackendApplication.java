2264073089fea6370435744b6ed3cf303031f708
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-27 18:55:10 -0500

    add @EnableEurekaClient

--- src/main/java/com/bank/msyankipaymentbackend/MsYankiPaymentBackendApplication.java
+++ src/main/java/com/bank/msyankipaymentbackend/MsYankiPaymentBackendApplication.java
@@ -2,7 +2,9 @@ package com.bank.msyankipaymentbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MsYankiPaymentBackendApplication {
 
